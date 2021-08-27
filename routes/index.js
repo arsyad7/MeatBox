82af2f98ff8dbe8e14a7e00182c6e952b0ba365f
arsyad7 <arsyadrama7@gmail.com>
2021-08-27 07:26:42 +0700

    final

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 const userRouter = require('./userRouter');
 const productRouter = require('./productRouter');
+const isLogin = require('../middlewares/checkLogin');
 
 router.get('/', (req, res) => {
     res.render('home')
 });
 
 router.use('/users', userRouter);
-router.use('/products', productRouter);
+
+router.use('/products', isLogin, productRouter);
 
 module.exports = router;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -7,4 +7,6 @@ router.post('/register', userController.register);
 router.get('/login', userController.loginForm);
 router.post('/login', userController.login);
 
+router.get('/logout', userController.logout);
+
 module.exports = router;
